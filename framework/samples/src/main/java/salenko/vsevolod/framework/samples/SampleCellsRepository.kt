46a2a0b4155074d33a2234af91ad34d267de770d
Vsevolod Salenko <vsalenko0@gmail.com>
2022-07-15 22:44:59 +0200

    Commit. Added cells generation functionality for SampleCellsRepository.kt. The getCells() function returns the list of cells with specified length or failure exception massage. The getCells() behavior is randomized.

--- framework/samples/src/main/java/salenko/vsevolod/framework/samples/SampleCellsRepository.kt
+++ framework/samples/src/main/java/salenko/vsevolod/framework/samples/SampleCellsRepository.kt
@@ -1,11 +1,64 @@
 package salenko.vsevolod.framework.samples
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 import salenko.vsevolod.domain.repositories.CellsRepository
 import salenko.vsevolod.entity.entity.Cell
+import kotlin.random.Random
 
 class SampleCellsRepository : CellsRepository {
-    override suspend fun getCells(): Result<List<Cell>> =
-        //TODO provide a sample list of the cells
-        Result.success(listOf(Cell("sample name", "sampleUrl")))
 
+    private val randTest by lazy { Random.Default }
+    private val testNames by lazy {
+        listOf<String>(
+            "Audi",
+            "fun nickname",
+            "very very very long name",
+            "Ford",
+            "not funny =)",
+            "Tesla",
+            "Simple name",
+            "Hyundai",
+            "Uvuvwevwevwe Onyetenyevwe Ugwemuhwem Osas",
+            "Smiling guy"
+        )
+    }
+
+    private val testPicURL by lazy {
+        listOf<String>(
+            "https://i1.sndcdn.com/artworks-000198393770-vdcdsj-t240x240.jpg",
+            "https://media.cntraveler.com/photos/5f15986ca107fd1a0223ddde/16:9/w_2560%2Cc_limit/MaineAcadiaNationalPark-2020-GettyImages-1065259808.jpg",
+            "https://img.freepik.com/premium–vector/austrian–black–tan-hound-dog-watercolor-sketch-hand-drawn-illustration_633389-37.jpg",
+            "https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg",
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Way_through_the_forest.jpg/1200px-Way_through_the_forest.jpg",
+            "https://images.all-free-download.com/images/graphiclarge/beauty_of_nature_17_211513.jpg",
+            "https://images.all-free-download.com/images/graphiclarge/beauty_of_nature_15_211311.jpg",
+            "https://static.wikia.nocookie.net/meme/images/5/52/Polite_Cat-0.jpg",
+            "https://images.theconversation.com/files/2770/original/orly.jpg",
+            "https://c.dns-shop.ru/thumb/st4/fit/300/219/3fdb344ccec0557e566f2a141b4452f4/q93_e470d5b1bb764724deb62d8ddb503921a67b9f9e06d46ac0e8afd694c7b5fd93.jpg",
+        )
+    }
+
+    override suspend fun getCells(): Result<List<Cell>> =
+        withContext(Dispatchers.IO) {
+            delay(randTest.nextLong(1, 10) * 1000)
+            if (randTest.nextBoolean()) {
+                val arrayList = ArrayList<Cell>()
+                for (i in 1..testPicURL.size) {
+                    arrayList.add(generateCell())
+                }
+                Result.success(
+                    arrayList
+                )
+            } else {
+                Result.failure(IllegalStateException("Sample exception"))
+            }
+
+        }
+
+    private fun generateCell() = Cell(
+        testNames[randTest.nextInt(testNames.size)],
+        testPicURL[randTest.nextInt(testPicURL.size)]
+    )
 }
